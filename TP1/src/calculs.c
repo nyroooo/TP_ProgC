9efd4e0de8071b918998d75f38496ca91e03fab6
garvanboulben <73408584+nyroooo@users.noreply.github.com>
2025-10-24 11:43:52 +0200

    Update calculs.c
    essai 1 Garvan

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,70 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 12;
+    int num2 = 5;
+    char op = '*';  
+
+    int resultat; 
+
+    printf("num1 = %d, num2 = %d, operateur = '%c'\n", num1, num2, op);
+
+    switch (op) {
+        case '+':
+            resultat = num1 + num2;
+            printf("Résultat : %d + %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '-':
+            resultat = num1 - num2;
+            printf("Résultat : %d - %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '*':
+            resultat = num1 * num2;
+            printf("Résultat : %d * %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '/':
+            if (num2 != 0) {
+                resultat = num1 / num2;
+                printf("Résultat : %d / %d = %d\n", num1, num2, resultat);
+            } else {
+                printf("Erreur : division par zéro impossible.\n");
+            }
+            break;
+
+        case '%':
+            if (num2 != 0) {
+                resultat = num1 % num2;
+                printf("Résultat : %d %% %d = %d\n", num1, num2, resultat);
+            } else {
+                printf("Erreur : modulo par zéro impossible.\n");
+            }
+            break;
+
+        case '&':
+            resultat = num1 & num2;
+            printf("Résultat : %d & %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '|':
+            resultat = num1 | num2;
+            printf("Résultat : %d | %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '~':
+           
+            resultat = ~num1;
+            printf("Résultat : ~%d = %d\n", num1, resultat);
+            break;
+
+        default:
+            printf("Erreur : opérateur '%c' non reconnu.\n", op);
+            break;
+    }
+
+    return 0;
+}
+
 
