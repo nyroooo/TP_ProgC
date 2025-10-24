c4c077f9c4ddb61af74da6a464d1788bbd24fbea
garvanboulben <73408584+nyroooo@users.noreply.github.com>
2025-10-24 10:44:55 +0200

    Update boucles.c
    essai 1 avec boucle while

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -1,26 +1,42 @@
 #include <stdio.h>
 
 int main() {
-    int compteur = 9; // Tu peux changer cette valeur (< 10)
+    int compteur = 5; 
+    int i = 1;
 
     if (compteur >= 10) {
         printf("Erreur : la valeur du compteur doit être inférieure à 10.\n");
         return 1;
     }
 
-    printf("Triangle rectangle avec des boucles for :\n\n");
+    printf("Triangle rectangle avec une boucle while :\n\n");
 
-    for (int i = 1; i <= compteur; i++) {
-        for (int j = 1; j <= i; j++) {
+    while (i <= compteur) {
+        int j = 1;
+        while (j <= i) {
             if (i > 2 && j > 1 && j < i) {
                 printf("# ");
             } else {
                 printf("* ");
             }
+
+            j++;
+
+            
+            if (j > i) {
+                continue;
+            }
         }
+
         printf("\n");
+        i++;
+
+        
+        if (i > compteur)
+            break;
     }
 
     return 0;
 }
+
 
