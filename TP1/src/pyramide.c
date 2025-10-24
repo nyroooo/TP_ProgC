4540a38328911e14549068fe54ebf20bd8f4cd84
garvanboulben <73408584+nyroooo@users.noreply.github.com>
2025-10-24 11:48:58 +0200

    Update pyramide.c
    essai 1 Garvan

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int n = 5;  
+    int i, j;
+
+    printf("Génération d'une pyramide de hauteur %d :\n\n", n);
+
+    
+    for (i = 1; i <= n; i++) {
+
+        
+        for (j = i; j < n; j++) {
+            printf(" ");
+        }
+
+     
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        
+        for (j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        
+        printf("\n");
+    }
+
+    printf("\nPyramide terminée !\n");
+
+    return 0;
+}
 
