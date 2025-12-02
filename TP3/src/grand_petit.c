efc5583c9f72ebcf9e7a08cb47329b708ba57cb6
garvanboulben <73408584+nyroooo@users.noreply.github.com>
2025-12-02 11:13:11 +0100

    grand_petit.c

---
+++ TP3/src/grand_petit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int tab[100];
+    srand(time(NULL));
+
+    for(int i = 0; i < 100; i++)
+        tab[i] = rand() % 1000 + 1;
+
+    int min = tab[0], max = tab[0];
+
+    for(int i = 0; i < 100; i++){
+        if(tab[i] < min) min = tab[i];
+        if(tab[i] > max) max = tab[i];
+    }
+
+    printf("Le numéro le plus petit est : %d\n", min);
+    printf("Le numéro le plus grand est : %d\n", max);
+
+    return 0;
+}
