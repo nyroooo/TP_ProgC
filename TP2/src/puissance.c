9873767c2288348aea7a068a6464c40d6cdec5e4
garvanboulben <73408584+nyroooo@users.noreply.github.com>
2025-12-02 09:50:04 +0100

    puissance.c

--- TP2/src/puissance.c
+++ TP2/src/puissance.c
@@ -1,33 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int a = 2;   // base
-    int b = 3;   // exposant
-    int resultat = 1; 
+    int a = 2;
+    int b = 3;
+    int result = 1;
 
-    printf("Calcul de la puissance : %d^%d\n", a, b);
-
-  
-    if (b == 0) {
-        resultat = 1;
-    } 
-    
-    else if (b > 0) {
-        for (int i = 1; i <= b; i++) {
-            resultat *= a;  
-        }
-    } 
-   
-    else {
-        double resultat_double = 1.0;
-        for (int i = 1; i <= -b; i++) {
-            resultat_double *= a;
-        }
-        printf("Résultat : 1/%.0f = %.6f\n", resultat_double, 1.0 / resultat_double);
-        return 0;
+    for(int i = 0; i < b; i++){
+        result *= a;
     }
 
-    printf("Résultat : %d^%d = %d\n", a, b, resultat);
-
+    printf("%d^%d = %d\n", a, b, result);
     return 0;
 }
